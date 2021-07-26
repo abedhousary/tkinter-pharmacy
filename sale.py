b3aff0a120bc9c369905795ac9a420829cb90725
abedhousary <boodihousar@gmail.com>
2021-07-26 13:36:26 +0300

    added total amount in sale window

--- sale.py
+++ sale.py
@@ -1,41 +1,55 @@
-from tkinter import  *
+from tkinter import *
 import mysql.connector
-def sales () :
-    class db :
+
+
+def sales():
+    class db:
         def __init__(self):
             self.cn = mysql.connector.connect(
-                host = "localhost",
-                user = "root",
-                password = "",
-                database = "pharmacy"
+                host="localhost",
+                user="root",
+                password="",
+                database="pharmacy"
             )
             self.cr = self.cn.cursor()
-        def getdata (self):
+
+        def getdata(self):
+            mon = 0
             self.cr.execute("select id,name,price from sells")
             fd = self.cr.fetchall()
             for row in fd:
-                s2 = "{:^85}{:^40}{:^110}".format(row[0],row[1],row[2])
-                lis.insert(END,s2)
+                s2 = "{:^85}{:^40}{:^110}".format(row[0], row[1], row[2])
+                mon += float(row[2])
+
+                lis.insert(END, s2)
+            e1.insert(0, str(int(mon)))
     Db = db()
     root = Tk()
     width = 800
     height = 500
     sw = root.winfo_screenwidth()
     sh = root.winfo_screenheight()
-    x = (sw / 2 ) - (width / 2)
-    y = (sh / 2)- (height /  2)
-    root.geometry("%dx%d+%d+%d"%(width,height,x,y))
+    x = (sw / 2) - (width / 2)
+    y = (sh / 2) - (height / 2)
+    root.geometry("%dx%d+%d+%d" % (width, height, x, y))
     root.config(bg="grey")
-    header = "{:>10}{:^130}{:^30}".format("Id","Name","Price")
-    lbl = Label(root,text = "Gains dashboard",bg="grey",font = ("fantasy",18))
-    lbl1 = Label(root,text = header ,bg="grey")
-    sc = Scrollbar(root,orient = "vertical")
+    header = "{:>10}{:^130}{:^30}".format("Id", "Name", "Price")
+    lbl = Label(root, text="Gains dashboard", bg="grey", font=("fantasy", 18))
+    lbl1 = Label(root, text=header, bg="grey")
+    sc = Scrollbar(root, orient="vertical")
 
-    lis = Listbox(root,yscrollcommand = sc.set,width=120,height = 26)
-    sc.config(command = lis.yview)
+    lis = Listbox(root, yscrollcommand=sc.set, width=120, height=26)
+    sc.config(command=lis.yview)
+    frame = Frame(root, height=90, width=850, bg="grey")
+    lb = Label(frame,fg="black",bg = "grey",text = "Total Amount",font=(12))
+    e1 = Entry(frame)
     lbl.pack()
     lbl1.pack()
-    sc.pack(side="right",fill = "y")
+    sc.pack(side="right", fill="y")
     lis.pack()
+    frame.pack(pady=2)
+    lb.place(x=490, y=1)
+    e1.place(x=620, y=3)
     Db.getdata()
     root.mainloop()
+
